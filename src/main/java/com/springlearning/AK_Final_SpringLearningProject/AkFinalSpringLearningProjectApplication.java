65657ec113e521cbd5663982253baffbc4da4641
AK-Malik <akmalik.it@gmail.com>
2025-08-23 10:25:40 -0400

    "First Commit"

---
+++ src/main/java/com/springlearning/AK_Final_SpringLearningProject/AkFinalSpringLearningProjectApplication.java
@@ -0,0 +1,15 @@
+package com.springlearning.AK_Final_SpringLearningProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AkFinalSpringLearningProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AkFinalSpringLearningProjectApplication.class, args);
+
+        System.out.println("Welcome to New SpringAPI Project");
+    }
+
+}
